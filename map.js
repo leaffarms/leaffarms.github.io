cc372087461249cba8fde5e0ef605141f3a7baa1
Joyce Chen <chenjy@mit.edu>
2019-07-12 03:13:40 -0400

    Modified the map object and interactions
    -Minor css changes and added icons

--- map.js
+++ map.js
@@ -1,11 +1,43 @@
-$("path").hover(function(e) {
-  $('#info-box').html($(this).data('info'));
-  console.log("yes");
+var active_paths = {
+	canada: {name: "Canada", produce: "Leafy veggies"},
+	mexico: {name: "Mexico", produce: "Mangosteen, Thai banana, Jackfruit"},
+	costa_rica: {name: "Costa Rica", produce: "Culantro"},
+	puerto_rico: {name: "Puerto Rico", produce: "Green mango"},
+	dominican_republic: {name: "Dominican Republic", produce: "Hot Thai pepper"},
+	honduras: {name: "Honduras", produce: "Asian vegetables, Indian okra, Rambutan"},
+	guatemala: {name: "Guatemala", produce: "Rambutan"},
+	florida: {name: "Florida", produce: "Leafy veggies"}
+};
+
+var disabled_paths = [
+	"united_states",
+	"nicaragua",
+	"Separator",
+	"panama",
+	"haiti",
+	"cuba_1",
+	"el_savador",
+	"belize"
+];
+
+fetch("map.html")
+.then(response => {
+	return response.text();
+})
+.then(data => {
+	document.getElementById("map-wrapper").innerHTML += data;
+	for (var path in active_paths) {
+		document.getElementById(path).classList.add("active");
+	}
+
+	for (var p in disabled_paths) {
+		document.getElementById(disabled_paths[p]).classList.add("disabled");
+	}
+
+	$("path").hover(function() {
+		var x = active_paths[this.id];
+		var title = "<div class=\"map-title\">" + x.name + "</div>";
+		var caption = "<div class=\"map-caption\">" + x.produce + "</div>";
+		document.getElementById("info-box").innerHTML = title + caption;
+	});
 });
-// $("path").mouseleave(function(e) {
-//   $('#info-box').css('display','none');
-// });
-// $(document).mousemove(function(e) {
-//   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-//   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-// }).mouseover();
